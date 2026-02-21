8af3998a244f41034a57ed232ae660dcbde9b8ed
mdmasum <mdmasumpresent@gmail.com>
2026-02-21 18:26:38 +0600

    latest

--- nj/src/app/layout.js
+++ nj/src/app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
       <body>
         {/* //////////////////////////// */}
         <nav className="bg-green-600 py-5 pl-5 border-0 border-blue-800 border-b-5">
-          <h1 className="text-white text-xl font-bold">Masum Software Foundation</h1>
+          <h1 className="text-white text-xl font-bold">Masum Software Lab</h1>
         </nav>
         {/* //////////////////////////// */}
         {children}
--- nj/src/app/tojbi/page.js
+++ nj/src/app/tojbi/page.js
@@ -6,11 +6,16 @@ const Tojbi = () => {
     const [tojbi,setTojbi] = useState('সুবহানাল্লাহ')
     const [valid,setValid] = useState(Boolean)
     const [user,setUser] = useState('')
+    const [step1,setStep1] = useState(1)
+    const [step2,setStep2] = useState(0)
+    const [step3,setStep3] = useState(0)
+    const [usernameLoad,setUsernameLoad] = useState('Guest User');
     //////////////////////////////
     useEffect(() => {
         const userName = localStorage?.getItem('username')
         if (userName){
             setValid(true)
+            setUsernameLoad(userName);
         }
     },[])
     //////////////////////////////
@@ -20,13 +25,19 @@ const Tojbi = () => {
             setCount(0)
         }
         if (count >= 0){
-            setTojbi('সুবহানাল্লাহ')
+            setTojbi(`সুবহানাল্লাহ ${step1}`)
+            setStep1(step1 + 1)
             if(count >= 32){
-                setTojbi('আলহামদুলিল্লাহ')
+                setTojbi(`আলহামদুলিল্লাহ ${step2}`)
+                setStep2(step2 + 1)
                 if (count >= 65){
-                    setTojbi('আল্লাহু আকবার')
+                    setTojbi(`আল্লাহু আকবার ${step3}`)
+                    setStep3(step3 + 1)
                     if (count >= 100){
-                        setTojbi('সুবহানাল্লাহ')
+                        setTojbi(`সুবহানাল্লাহ`)
+                        setStep3(0)
+                        setStep1(1)
+                        setStep2(0)
                     }
                 }
             }
@@ -40,8 +51,14 @@ const Tojbi = () => {
         window.location.reload()
     }
     //////////////////////////////
+    const RemoveUser = () => {
+        localStorage?.removeItem('username')
+        window.location.reload()
+    }
+    //////////////////////////////
     return(
-        <main className="pt-20 px-12">
+        <main className="pt-15 px-12">
+            {valid && <div className="flex justify-center text-center mb-10 text-xl">User- <h1 className="font-bold text-blue-700">{usernameLoad}</h1><button onClick={RemoveUser} className="ml-5 bg-red-600 text-white px-3 rounded-lg pb-1 hover:bg-red-700 cursor-pointer">Remove</button></div>}
             {valid && <div className="flex justify-center"><h1 className="bg-green-300 py-5 px-5 rounded-xl text-2xl font-bold">{tojbi}</h1></div>}
             {valid && <div className="border-2 border-b-blue-500 bg-amber-500 mt-18 rounded-2xl flex flex-col justify-center items-center h-60 gap-10">
                 <h1 className="text-4xl bg-blue-800 text-white p-2 rounded-2xl">{count}</h1>
